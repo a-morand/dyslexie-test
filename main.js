cfae0f6e7a09cb85d695eea659ff7b221bec5026
Axel <axel.morand@hetic.net>
2016-12-07 11:34:27 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,63 @@
+document.addEventListener('DOMContentLoaded', function(){
+	 $( "#r1" ).click(function() {
+  	 	responsiveVoice.speak(#r1.toString(), "French Female");
+	 });
+
+	  
+      $(function() {
+        // Dissimulation des réponses
+        $('.reponse').hide();
+        
+        // Mise en forme des div du QCM
+        var q = $('.question');
+        q.css('background', '#9EEAE0');
+        q.css('border-style', 'groove');
+        q.css('border-width', '4px');
+        q.css('width', '900px');
+        q.css('height', '250px');
+        q.css('margin', '20px');
+
+        $('.texte').css('float', 'left');
+        $('.texte').css('width', '90%');
+        $('img').css('float', 'right');
+        $('img').css('margin-top', '80px');
+        
+        // Action au survol du lien « Tester les réponses »
+        $('a').hover(
+          function() { 
+            $('.reponse').show();
+            if ($(':radio[id="r1"]:checked').val()) {
+              $('#img1').attr('src', 'bon.png'); 
+              $('#reponse1').css('color', 'green');
+            }  
+            else {
+              $('#img1').attr('src', 'mauvais.png');
+              $('#reponse1').css('color', 'red');
+            } 
+            if ($(':radio[id="r4"]:checked').val()) {
+              $('#img2').attr('src', 'bon.png');
+              $('#reponse2').css('color', 'green');
+            }
+            else {
+              $('#img2').attr('src', 'mauvais.png');
+              $('#reponse2').css('color', 'red');
+            } 
+            if ($(':radio[id="r8"]:checked').val()) {
+              $('#img3').attr('src', 'bon.png'); 
+              $('#reponse3').css('color', 'green');
+            }
+            else {
+              $('#img3').attr('src', 'mauvais.png');
+              $('#reponse3').css('color', 'red');
+            }
+          },
+          function() { 
+            $('.reponse').hide(); 
+            $('img').each(function() {
+              $(this).attr('src', 'question.png');
+          });
+}
+
+        );  
+      });
+})

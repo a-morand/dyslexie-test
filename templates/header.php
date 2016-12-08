d9dbe576bda30de54ba92957f3099a83f2401725
Axel <axel.morand@hetic.net>
2016-12-08 14:53:25 +0100

    CSS FOLDERS

--- templates/header.php
+++ templates/header.php
@@ -2,13 +2,16 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<link rel="stylesheet" href="../src/styles/main.css" />
-<!-- Important Owl stylesheet -->
-<link rel="stylesheet" href="../libs/owl.carousel.css">
-<!-- Default Theme -->
-<link rel="stylesheet" href="../libs/owl.theme.css">
-<!-- Jquery -->
-<title>Questionnaire interactif en jQuery</title>
+	<link rel="stylesheet" href="../src/styles/header.css" />
+	<link rel="stylesheet" href="../src/styles/footer.css" />
+	<link rel="stylesheet" href="../src/styles/home.css" />
+	<link rel="stylesheet" href="../src/styles/main.css" />
+	<!-- Important Owl stylesheet -->
+	<link rel="stylesheet" href="../libs/owl.carousel.css">
+	<!-- Default Theme -->
+	<link rel="stylesheet" href="../libs/owl.theme.css">
+	<!-- Jquery -->
+	<title>Questionnaire interactif en jQuery</title>
 </head>
 <body>
 <header>

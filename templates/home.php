5437c3ddfb9b7b4c27aa78ae0128a1bd361c5324
timothe3192 <blanco.timothe@gmail.com>
2016-12-08 15:42:23 +0100

    Add home css + php

--- templates/home.php
+++ templates/home.php
@@ -1,3 +1,11 @@
 <?php require_once "header.php"; ?>
+    <nav class="nav"></nav>
+    <div class="container"></div>
+        <div class="container__icon">
+            <img src="" alt="Logo">
+        </div>
+        <div class="text">Bienvenue sur Babou.</div>
+
+        <div class="button"><span class="button__text">Commencez une partie</span></div>
 
 <?php require_once "footer.php"; ?>
